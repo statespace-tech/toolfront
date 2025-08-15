a3559617e8877c8c1d8c3be581e14e08e4f0b92f
antidmg <antidmg@proton.me>
2025-08-15 16:07:45 -0400

    fix: add Google gtag script for proper analytics tracking

---
+++ docs/assets/js/gtag-init.js
@@ -0,0 +1,5 @@
+// Google Analytics gtag initialization
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-EH352HTY05');
