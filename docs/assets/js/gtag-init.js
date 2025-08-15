8a9057aa3e78152e98c117da1118b08f4f27ccb3
gavin <antidmg@proton.me>
2025-08-15 21:54:03 +0100

    feat: add Google Analytics tracking for documentation insights (#50)

--- docs/assets/js/gtag-init.js
+++ docs/assets/js/gtag-init.js
@@ -1,5 +1,15 @@
-// Google Analytics gtag initialization
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-EH352HTY05');
+// Google Analytics initialization - load script dynamically since external URLs don't work
+(function() {
+    // Load the gtag script first
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-EH352HTY05';
+    document.head.appendChild(script);
+    
+    // Initialize gtag
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', 'G-EH352HTY05');
+})();
